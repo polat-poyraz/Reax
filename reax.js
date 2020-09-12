ee79f8dad945a871b5e0c425f27fb9e0d33513ad
polatpoyraz-1 <polat64polat34@gmail.com>
2020-09-12 13:59:55 +0300

    Reax foundation finished

---
+++ reax.js
@@ -0,0 +1,54 @@
+class ReaxMain {
+    constructor (state, methods, historyMode){
+        this.contetnControls = {
+            history: historyMode
+        }
+        this.state = state
+        this.methods = methods
+        this.promise = new Promise((resolve, reject) => { resolve(this); reject('Opps.')})
+        this.historyData = []
+
+        // lifecycle
+        this.methods.firedBefore(this.promise)
+    }
+    updateState (param){
+        for (const prop in param){
+            if (this.state[prop] === undefined){
+                this.state[prop] = param[prop]
+                this.history('add', prop)
+            } else {
+                this.state[prop] = param[prop]
+                this.history('update', prop)
+            }
+        }
+    }
+    removeState (prop){
+        for (let i = 0; i < prop.length; i++){
+            if(this.state[prop[i]] !== undefined){
+                this.history('remove', prop)
+                delete this.state[prop[i]]
+            }
+        }
+    }
+    history (mode, prop){
+        if (this.contetnControls.history || this.contetnControls.history === undefined){
+            let after = {}
+            if (mode === 'add'){
+                after['history'] = `${mode} = ${prop}: ${this.state[prop]}`
+            } else if (mode === 'update'){
+                after['history'] = `${mode} = ${prop}: ${this.state[prop]}`
+            } else if (mode === 'remove'){
+                after['history'] = `${mode} = ${prop[0]}: ${this.state[prop[0]]}`
+            }
+            this.historyData.push(after)
+
+            if (this.historyData.length >= 2){
+                if (this.historyData[this.historyData.length-1].history === this.historyData[this.historyData.length-2].history){
+                    this.historyData.splice((this.historyData.length-1), 1)
+                }
+            } 
+        }
+    }
+}
+
+const Reax = new ReaxMain(/*state*/{}, /*methods*/{}, /*history*/true)
